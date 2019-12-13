5393c97e788e74260ea7c1a8f7bf89ef0deb9454
charlie <bean17.cha@gmail.com>
2019-12-13 16:27:23 +0800

    update CuidUtilsTest

--- kotlindemo/src/test/kotlin/org/charclie/CuidUtilsTest.kt
+++ kotlindemo/src/test/kotlin/org/charclie/CuidUtilsTest.kt
@@ -2,6 +2,7 @@ package org.charclie
 
 import org.charlie.CuidUtils
 import org.junit.Test
+import java.io.File
 
 class CuidUtilsTest {
     @Test
@@ -13,13 +14,28 @@ class CuidUtilsTest {
 
     @Test
     fun decoded() {
-        val s = "我爱北京天安门"
-        println(s.toByteArray().size)
-
         val encode = "EE49C4-DFF51B-C31BG8-F83344-BD2D20-G7"
         println(CuidUtils.decode(encode))
 
         val encode2 = "7CG982-6BFG46-E5C61F-2387G9-578263-1B"
         println(CuidUtils.decode(encode2))
+    }
+
+    @Test
+    fun decodeFile() {
+        val set = HashSet<String>()
+        val inFile = File("d:/input.txt")
+        val lines = inFile.readLines()
+        val outFile = File("d:/output.txt")
+        outFile.delete()
+        for (line in lines) {
+            if (set.contains(line)) {
+                continue
+            }
+
+            set.add(line)
+            outFile.appendText(CuidUtils.decode(line))
+            outFile.appendText("\n")
+        }
     }
 }
